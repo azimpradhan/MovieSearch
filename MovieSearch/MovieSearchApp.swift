bbb37d908404f4dbe1975b2932eace9838af7e57
Azim Pradhan <azim.pradhan@gmail.com>
2025-04-03 13:06:18 -0700

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Azim Pradhan on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
